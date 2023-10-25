b1305ad60c8cbe1d0614cab7a0b3e980821967fb
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 16:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+// function convertToRoman(num) {
+//   const obj = {
+//     0: ['M', 1000],
+//     1: ['D', 500],
+//     2: ['C', 100],
+//     3: ['L', 50],
+//     4: ['X', 10],
+//     5: ['V', 5],
+//     6: ['I', 1]
+//   };
 
-  let result = '';
+//   let result = '';
 
-  for (let key in obj) {
-    const [numeral, value] = obj[key];
-    while (num >= value) {
-      result += numeral;
-      num -= value;
-    }
-  }
+//   for (let key in obj) {
+//     const [numeral, value] = obj[key];
+//     while (num >= value) {
+//       result += numeral;
+//       num -= value;
+//     }
+//   } 
 
-  return result;
-}
+//   alert(result);
+// }
 
-// Take input from the user
-const userInput = prompt("Enter a number between 0 and 100000:");
-const inputNumber = parseInt(userInput, 10);
+// // Take input from the user
+// const userInput = prompt("Enter a number between 0 and 100000:");
+// const inputNumber = parseInt(userInput, 10);
 
-// Check if the input is a valid number
-if (!isNaN(inputNumber) && inputNumber >= 0 && inputNumber <= 100000) {
-  const romanNumeral = convertToRoman(inputNumber);
-  console.log(`Roman Numeral: ${romanNumeral}`);
-} else {
-  console.log("Invalid input. Please enter a number between 0 and 100000.");
-}
+// // Check if the input is a valid number
+// if (!isNaN(inputNumber) && inputNumber >= 0 && inputNumber <= 100000) {
+//   const romanNumeral = convertToRoman(inputNumber);
+//   console.log(`Roman Numeral: ${romanNumeral}`);
+// } else {
+//   console.log("Invalid input. Please enter a number between 0 and 100000.");
+// }
